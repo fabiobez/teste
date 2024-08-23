cf88add48052b82477bb35b619dddda748fa1235
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-23 18:50:43 -0300

    s

--- customActivity.js
+++ customActivity.js
@@ -4,7 +4,34 @@ define(['postmonger'], function(postmonger) {
     var lastStepEnabled = false;
 
     $(window).ready(onRender);
-
+  
     connection.on("initActivity", initialize);
+    connection.on("requestedTokens", onGetTokens);
+    connection.on("requestedEndpoints", onGetEndpoints);
+  
+    connection.on("clickedNext", onClickedNext);
+    connection.on("clickedBack", onClickedBack);
+    connection.on("gotoStep", onGotoStep);
+  
+    function onRender() {
+      // JB will respond the first time 'ready' is called with 'initActivity'
+      connection.trigger("ready");
+  
+      connection.trigger("requestTokens");
+      connection.trigger("requestEndpoints");
+  
+      // Disable the next button if a value isn't selected
+      $("#select1").change(function () {
+        var message = getMessage();
+        connection.trigger("updateButton", {
+          button: "next",
+          enabled: Boolean(message),
+        });
+  
+        $("#message").html(message);
+      });  
+
+    }
+
 
 });
