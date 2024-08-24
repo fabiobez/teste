8208886634774d70c1aa4759300fe36c6237b1af
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 09:01:38 -0300

    d

--- customActivity.js
+++ customActivity.js
@@ -26,20 +26,10 @@ define(["postmonger"], function (Postmonger) {
     
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger("ready");
-    
+        connection.trigger("ready");    
         connection.trigger("requestTokens");
-        connection.trigger("requestEndpoints");
-    
-        // Disable the next button if a value isn't selected
-        $("#select1").change(function () {
-          var message = getMessage();
-          connection.trigger("updateButton", {
-            button: "next",
-            enabled: Boolean(message),
-          });
-    
-          $("#message").html(message);
+        connection.trigger("requestEndpoints");    
+        
         });  
   
       }
