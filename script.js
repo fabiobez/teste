a6de4b6b335c6dcc7916e5e54331484b835a10a3
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 15:02:05 -0300

    x

--- script.js
+++ script.js
@@ -23,16 +23,16 @@ function moverParaEsquerda() {
     const listaEsquerda = document.getElementById('idfield');
     const listaDireita = document.getElementById('listaDireita');
 
-    // Percorre todas as opções selecionadas na lista da esquerda
+    // Percorre todas as opções selecionadas na lista da direita
     for (let i = 0; i < listaDireita.options.length; i++) {
         if (listaDireita.options[i].selected) {
-            // Cria uma nova opção na lista da direita com o mesmo valor e texto
+            // Cria uma nova opção na lista da esquerda com o mesmo valor e texto
             const novaOpcao = document.createElement('option');
             novaOpcao.value = listaDireita.options[i].value;
             novaOpcao.text = listaDireita.options[i].text;
             listaEsquerda.add(novaOpcao);
 
-            // Remove a opção da lista da esquerda
+            // Remove a opção da lista da direita
             listaDireita.remove(i);
             // Ajusta o índice para não pular elementos após a remoção
             i--;
