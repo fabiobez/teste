0f855ace3140cf0eed71422a392d58a3c0c982cd
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 12:11:35 -0300

    d

--- customActivity.js
+++ customActivity.js
@@ -57,6 +57,31 @@ define(["postmonger"], function (Postmonger) {
         let argArr = payload['arguments'].execute.inArguments;
       
       }
+
+      function moverParaDireita() {
+        const listaEsquerda = document.getElementById('listaEsquerda');
+        const listaDireita = document.getElementById('listaDireita');
+    
+        // Percorre todas as opções selecionadas na lista da esquerda
+        for (let i = 0; i < listaEsquerda.options.length; i++) {
+            if (listaEsquerda.options[i].selected) {
+                // Cria uma nova opção na lista da direita com o mesmo valor e texto
+                const novaOpcao = document.createElement('option');
+                novaOpcao.value = listaEsquerda.options[i].value;
+                novaOpcao.text = listaEsquerda.options[i].text;
+                listaDireita.add(novaOpcao);
+    
+                // Remove a opção da lista da esquerda
+                listaEsquerda.remove(i);
+                // Ajusta o índice para não pular elementos após a remoção
+                i--;
+            }
+        }
+    }
+    
+    function moverParaEsquerda() {
+        // Lógica similar para mover da direita para a esquerda, com as operações invertidas
+    }
     
     
       function onClickedNext() {
