1e833e7bd0ecf618566ef275430d39825fc6e2a6
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 16:12:01 -0300

    wewe

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
 function moverParaDireita() {
-    const listaEsquerda = document.getElementById('idfield');
+    
+    const listaEsquerda = document.getElementById('listaEsquerda');
     const listaDireita = document.getElementById('listaDireita');
 
-    var selectedOpts = $("#idField option:selected");
-    console.log(selectedOpts);
-
     // Percorre todas as opções selecionadas na lista da esquerda
-    for (let iop = 1; iop < selectedOpts; iop++) {
-        if (selectedOpts[iop]) {
-
-            console.log(selectedOpts[iop]);
+    for (let i = 0; i < listaEsquerda.options.length; i++) {
+        if (listaEsquerda.options[i].selected) {
             // Cria uma nova opção na lista da direita com o mesmo valor e texto
             const novaOpcao = document.createElement('option');
-            novaOpcao.value = selectedOpts[iop].value;
-            novaOpcao.text = selectedOpts[iop].text;
-            listaDireita.add(selectedOpts);
+            novaOpcao.value = listaEsquerda.options[i].value;
+            novaOpcao.text = listaEsquerda.options[i].text;
+            listaDireita.add(novaOpcao);
 
             // Remove a opção da lista da esquerda
-            listaEsquerda.remove(iop);
+            listaEsquerda.remove(i);
             // Ajusta o índice para não pular elementos após a remoção
-            iop--;
+            i--;
         }
     }
 }
 
 function moverParaEsquerda() {
-    const listaEsquerda = document.getElementById('idfield');
-    const listaDireita = document.getElementById('listaDireita');
-
-    // Percorre todas as opções selecionadas na lista da direita
-    for (let i = 0; i < listaDireita.options.length; i++) {
-        if (listaDireita.options[i].selected) {
-            // Cria uma nova opção na lista da esquerda com o mesmo valor e texto
-            const novaOpcao = document.createElement('option');
-            novaOpcao.value = listaDireita.options[i].value;
-            novaOpcao.text = listaDireita.options[i].text;
-            listaEsquerda.add(novaOpcao);
-
-            // Remove a opção da lista da direita
-            listaDireita.remove(i);
-            // Ajusta o índice para não pular elementos após a remoção
-            i--;
-        }
-    }
-
+    // Lógica similar para mover da direita para a esquerda, com as operações invertidas
 }
