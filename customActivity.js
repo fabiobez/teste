72f4e97e9ee229f4ca00aa738c6e28d6e79cd6f2
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 09:05:24 -0300

    d

--- customActivity.js
+++ customActivity.js
@@ -30,8 +30,7 @@ define(["postmonger"], function (Postmonger) {
         connection.trigger("requestTokens");
         connection.trigger("requestEndpoints");    
         
-        });  
-  
+        
       }
   
 
