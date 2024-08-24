dd20b35630105b00dc769ebc48ca00b5a1e02c99
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-23 21:04:10 -0300

    a

--- customActivity.js
+++ customActivity.js
@@ -35,5 +35,34 @@ define(["postmonger"], function (Postmonger) {
         message.textContent = "Hello world";
     }
 
+    connection.on("requestedTriggerEventDefinition", function(
+        eventDefinitionModel
+    ) {
+        if (eventDefinitionModel) {
+            definition = eventDefinitionModel;
+            eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+        }
+    });
+    
+    function save() {
+        var campaign_id = $("#campaign_id").val();
+        payload["arguments"].execute.inArguments = [{
+            campaign_id: campaign_id,
+            recipient: {
+                first_name: "{{Event." + eventDefinitionKey + '."first_name"}}',
+                last_name: "{{Event." + eventDefinitionKey + '."last_name"}}',
+                company: "{{Event." + eventDefinitionKey + '."company"}}',
+                email: "{{Event." + eventDefinitionKey + '."email"}}',
+                address_1: "{{Event." + eventDefinitionKey + '."address_1"}}',
+                city: "{{Event." + eventDefinitionKey + '."city"}}',
+                state: "{{Event." + eventDefinitionKey + '."state"}}',
+                postal_code: "{{Event." + eventDefinitionKey + '."postal_code"}}',
+                identifier: "{{Event." + eventDefinitionKey + '."identifier"}}'
+            }
+        }];
+        payload["metaData"].isConfigured = true;
+        connection.trigger("updateActivity", payload);
+    }
+
 
 });
