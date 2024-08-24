222628cc0ea73e3dddc0a8969ad7bea818309ede
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-24 13:27:50 -0300

    include script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+function moverParaDireita() {
+    const listaEsquerda = document.getElementById('idfield');
+    const listaDireita = document.getElementById('listaDireita');
+
+    // Percorre todas as opções selecionadas na lista da esquerda
+    for (let i = 0; i < listaEsquerda.options.length; i++) {
+        if (listaEsquerda.options[i].selected) {
+            // Cria uma nova opção na lista da direita com o mesmo valor e texto
+            const novaOpcao = document.createElement('option');
+            novaOpcao.value = listaEsquerda.options[i].value;
+            novaOpcao.text = listaEsquerda.options[i].text;
+            listaDireita.add(novaOpcao);
+
+            // Remove a opção da lista da esquerda
+            listaEsquerda.remove(i);
+            // Ajusta o índice para não pular elementos após a remoção
+            i--;
+        }
+    }
+}
+
+function moverParaEsquerda() {
+    const listaEsquerda = document.getElementById('idfield');
+    const listaDireita = document.getElementById('listaDireita');
+
+    // Percorre todas as opções selecionadas na lista da esquerda
+    for (let i = 0; i < listaDireita.options.length; i++) {
+        if (listaDireita.options[i].selected) {
+            // Cria uma nova opção na lista da direita com o mesmo valor e texto
+            const novaOpcao = document.createElement('option');
+            novaOpcao.value = listaDireita.options[i].value;
+            novaOpcao.text = listaDireita.options[i].text;
+            listaEsquerda.add(novaOpcao);
+
+            // Remove a opção da lista da esquerda
+            listaDireita.remove(i);
+            // Ajusta o índice para não pular elementos após a remoção
+            i--;
+        }
+    }
+
+}
