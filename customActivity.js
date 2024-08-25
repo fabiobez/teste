eedc84b9277ac716ba4276d861b2fb31b9ccc016
fabiobez <40800092+fabiobez@users.noreply.github.com>
2024-08-25 16:16:57 -0300

    asd

--- customActivity.js
+++ customActivity.js
@@ -23,7 +23,7 @@ define(["postmonger"], function (Postmonger) {
       connection.trigger("ready");  
       
           // Disable the next button if a value isn't selected
-          $("#listaDireita","#listaDireita").change(function () {   
+          $("#moveright").onclick(function () {   
             var message = getMessage();
             console.log('MESSAGE:' + message);         
             connection.trigger("updateButton", {
